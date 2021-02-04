1c0a91e11ab72ec93df53f8b5ff63705729d2463
ackhare <bairagisirfire@gmail.com>
2021-02-04 12:50:36 +0530

    Update apputil.ts

--- apputil.ts
+++ apputil.ts
@@ -1,1 +1,9 @@
-
+  convertToDateFormatFromMilliSeconds(date: any) {
+    if (date == undefined || date == null || date == '')
+      return null;
+    let date1 = new Date(date); // Date 2011-05-09T06:08:45.178Z
+    let year1 = date1.getFullYear();
+    let month1 = ("0" + (date1.getMonth() + 1)).slice(-2);
+    let day1 = ("0" + date1.getDate()).slice(-2);
+    return `${month1}/${day1}/${year1}`;
+  }
